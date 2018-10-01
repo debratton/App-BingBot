54f7b00f13f6546c3d67ce1c449026695610c2d6
debratton <debratton@gmail.com>
2018-10-01 13:54:42 -0500

    complete

--- App-BingBot/App-BingBot/ViewController.swift
+++ App-BingBot/App-BingBot/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var showsLbl: UILabel!
+    @IBOutlet weak var randomShowLbl: UILabel!
+    @IBOutlet weak var bingeBotSpokenLbl: UILabel!
+    @IBOutlet weak var addShowTxt: UITextField!
+    @IBOutlet weak var addShowBtn: UIButton!
+    @IBOutlet weak var whatToBingeBtn: UIButton!
+    @IBOutlet weak var randomShowSV: UIStackView!
+    @IBOutlet weak var addShowSV: UIStackView!
+    @IBOutlet weak var showsSV: UIStackView!
+    
+    // DECLARE EMPTY ARRAY OR TYPE STRING
+    var shows: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showsSV.isHidden = true
+        randomShowSV.isHidden = true
+        addShowBtn.isEnabled = false
     }
-
-
+    
+ 
+    @IBAction func addShowTxtChanged(_ sender: UITextField) {
+        if sender.text! != "" {
+            addShowBtn.isEnabled = true
+        } else {
+            addShowBtn.isEnabled = false
+        }
+    }
+    
+    @IBAction func whatToBingeBtnPressed(_ sender: Any) {
+        randomShowLbl.text = shows.randomElement()
+        randomShowLbl.isHidden = false
+        bingeBotSpokenLbl.isHidden = false
+    }
+    
+    func updateShowsLbl() {
+        showsLbl.text = shows.joined(separator: ", ")
+    }
+    
+    @IBAction func addShowBtnPressed(_ sender: Any) {
+        guard let showName = addShowTxt.text else {return}
+        shows.append(showName)
+        addShowTxt.text = ""
+        addShowBtn.isEnabled = false
+        updateShowsLbl()
+        showsSV.isHidden = false
+        
+        if shows.count >= 2 {
+            randomShowSV.isHidden = false
+            randomShowLbl.isHidden = true
+            bingeBotSpokenLbl.isHidden = true
+        }
+    }
 }
 
